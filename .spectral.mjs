4ef9bd69d98188eef93d738aebf70236fc214e75
Pierre Le Guen <pierre.leguen@koust.net>
2024-02-09 09:55:07 +0100

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMzNTQ5/spectral.js?branch_slug=master&token=5b782056-b45c-4dc1-a443-7d6b2c5809b2";
+export default { extends: ruleset };
